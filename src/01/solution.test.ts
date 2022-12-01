4a4e28556ecccecc951cbcc7f81fc4633241f8e5
Olie Chan <olie.chan@nearform.com>
2022-12-01 19:29:25 +0000

    parse file

--- src/01/solution.test.ts
+++ src/01/solution.test.ts
@@ -1,27 +1,23 @@
-import { getMaxTotal } from "./solution";
+import { getMaxTotal, parseFileInput } from "./solution";
+import path from "path";
 
 it("returns the biggest total of a group", () => {
-	const input =  [
-		[
-			1000,
-			2000,
-			3000,
-		],
-		[
-			4000
-		],
-		[
-			5000,
-			6000
-		],
-		[
-			7000,
-			8000,
-			9000,
-		],
-		[
-			10000
-		]
+	const input = [
+		[1000, 2000, 3000],
+		[4000],
+		[5000, 6000],
+		[7000, 8000, 9000],
+		[10000],
 	];
 	expect(getMaxTotal(input)).toEqual(24000);
+});
+
+it("parses a string into a list of numbers", () => {
+	expect(parseFileInput(path.join(__dirname, "./sample.txt"))).toStrictEqual([
+		[1000, 2000, 3000],
+		[4000],
+		[5000, 6000],
+		[7000, 8000, 9000],
+		[10000],
+	]);
 });
--- src/01/solution.ts
+++ src/01/solution.ts
@@ -1,5 +1,25 @@
+import { readFileSync } from "fs";
+
 function getMaxTotal(list: number[][]): number {
-	return 0;
+	let max = -Infinity;
+	list.forEach(group => {
+		const total = sum(group);
+		if (total > max) {
+			max = total;
+		}
+	});
+
+	return max;
 }
 
-export { getMaxTotal };
+const sum = (numbers: number[]): number  =>
+	numbers.reduce((total, number) => total + number, 0);
+
+const parseFileInput = (path: string): number[][] => {
+	const contents = readFileSync(path, "utf8").trim();
+	const groups =  contents.split(/[\n]{2,}/g);
+	const parsedGroups = groups.map(group => group.split("\n").map(line => parseInt(line, 10)));
+	return parsedGroups;
+};
+
+export { getMaxTotal, parseFileInput };
