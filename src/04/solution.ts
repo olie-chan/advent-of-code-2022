5d2a96d11137cb8ac24209d8f142c1622cbde046
Olie Chan <olie.chan@nearform.com>
2022-12-04 17:04:58 +0000

    formatting

--- src/04/solution.ts
+++ src/04/solution.ts
@@ -3,7 +3,8 @@ import { readFileSync } from "fs";
 //https://adventofcode.com/2022/day/4
 type Range = [number, number];
 
-const parseRange = (line: string): Range[] => line.split(",").map(range => range.split("-").map(Number) as Range);
+const parseRange = (line: string): Range[] =>
+	line.split(",").map((range) => range.split("-").map(Number) as Range);
 
 const parseFileInput = (path: string): Range[][] => {
 	const lines = readFileSync(path, "utf8").trim().split("\n");
@@ -11,7 +12,8 @@ const parseFileInput = (path: string): Range[][] => {
 };
 
 // a fits into b?
-const isInclusive = (a: Range, b: Range): boolean => a[0] >= b[0] && a[1] <= b[1];
+const isInclusive = (a: Range, b: Range): boolean =>
+	a[0] >= b[0] && a[1] <= b[1];
 
 const listHasInclusiveRange = (list: Range[]): boolean => {
 	let result = false;
@@ -19,11 +21,13 @@ const listHasInclusiveRange = (list: Range[]): boolean => {
 		if (result) {
 			return result;
 		}
-		result = list.slice(0, i).concat(list.slice(i + 1)).some(range => isInclusive(list[i], range));
+		result = list
+			.slice(0, i)
+			.concat(list.slice(i + 1))
+			.some((range) => isInclusive(list[i], range));
 	}
 	return result;
 };
-
 
 const getTotalInclusiveRanges = (input: Range[][]): number => {
 	return input.map(listHasInclusiveRange).filter(Boolean).length;
