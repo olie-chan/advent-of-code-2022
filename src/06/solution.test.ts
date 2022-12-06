91abda1ecff2d337b71cf61ba66437f5db9a07f1
Olie Chan <olie.chan@nearform.com>
2022-12-06 18:28:53 +0000

    answer question

--- src/06/solution.test.ts
+++ src/06/solution.test.ts
@@ -1,6 +1,15 @@
 import { getMarker } from "./solution";
+import path from "path";
+import {readFileSync} from "fs";
+
+const parseFileInput = (path: string): string =>
+	readFileSync(path, "utf-8").trim();
+
+const input = parseFileInput(path.join(__dirname, "input.txt"));
 
 it("returns the marker for when there have been 4 unique characters", () => {
-	expect(getMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toBe(7);
-	expect(getMarker("bvwbjplbgvbhsrlpgdmjqwftvncz")).toBe(5);
+	expect(getMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4)).toBe(7);
+	expect(getMarker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4)).toBe(5);
+	expect(getMarker(input, 4)).toBe(1779);
+	expect(getMarker(input, 14)).toBe(2635);
 });
--- src/06/solution.ts
+++ src/06/solution.ts
@@ -1,7 +1,13 @@
 //https://adventofcode.com/2022/day/6
 
-const getMarker = (input: string): number =>  {
-	return 0;
+const getMarker = (input: string, uniqueCount: number): number => {
+	return [...input].findIndex((_, i, arr) => {
+		const fragment: string[] = arr.slice(i - uniqueCount, i);
+		const set = new Set(fragment);
+		return (
+			(fragment.length === uniqueCount && fragment.length === set.size)
+		);
+	});
 };
 
 export { getMarker };
