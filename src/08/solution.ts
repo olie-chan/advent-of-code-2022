70e74b2285b32dec820a2de8aecda6c60bd46356
Olie Chan <olie.chan@nearform.com>
2022-12-08 22:03:11 +0000

    fix initial condition

--- src/08/solution.ts
+++ src/08/solution.ts
@@ -2,37 +2,34 @@ import { readFileSync } from "fs";
 // https://adventofcode.com/2022/day/8
 const readFile = (file: string) => readFileSync(file, "utf8").trim();
 
-const parseInput = (input: string) => input.split("\n").map(line => line.split("").map(Number));
+const parseInput = (input: string) =>
+	input.split("\n").map((line) => line.split("").map(Number));
 
 const parseFile = (file: string) => parseInput(readFile(file));
 
-
 const getTreesAbove = (trees: number[][], y: number, x: number) =>
-	trees.slice(0, y).map(row => row[x]);
+	trees.slice(0, y).map((row) => row[x]);
 const getTreesBelow = (trees: number[][], y: number, x: number) =>
-	trees.slice(y + 1).map(row => row[x]);
+	trees.slice(y + 1).map((row) => row[x]);
 const getTreesLeft = (trees: number[][], y: number, x: number) =>
 	trees[y].slice(0, x);
 const getTreesRight = (trees: number[][], y: number, x: number) =>
 	trees[y].slice(x + 1);
 
-
 const isVisible = (trees: number[][], y: number, x: number) => {
 	const tree = trees[y][x];
-	const compareFn = (t: number, c: number) => t > c ? c : Infinity;
-	const visibileMap = [getTreesAbove, getTreesBelow, getTreesLeft, getTreesRight].map(f => Number.isFinite(f(trees, y, x).reduce(compareFn, tree)));
-	visibileMap.forEach((v, i) => {
-		if (v) {
-			console.log({ tree, y, x, visibleFrom: ["above", "below", "left", "right"][i], v});
-		}
-	});
-	return visibileMap.some(v => Boolean(v));
+	const compareFn = (t: number, c: number) => (c < t ? c : -Infinity);
+	const visibileMap = [
+		getTreesAbove,
+		getTreesBelow,
+		getTreesLeft,
+		getTreesRight,
+	].map((f) => f(trees, y, x).reduce(compareFn, tree));
+	return visibileMap.some((v) => Number.isFinite(v));
 };
 
 const countVisibleTrees = (trees: number[][]) => {
 	let count = 0;
-	const [yMin, yMax] = [0, trees.length-1];
-	const [xMin, xMax] = [0, trees[0].length-1];
 	for (let i = 0; i < trees.length; i++) {
 		for (let j = 0; j < trees[i].length; j++) {
 			if (isVisible(trees, i, j)) {
