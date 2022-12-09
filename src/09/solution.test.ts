677e29f003d4f207f0a434db39e07079c91c75e2
Olie Chan <olie.chan@nearform.com>
2022-12-09 19:47:37 +0000

    get history of head travelled

--- src/09/solution.test.ts
+++ src/09/solution.test.ts
@@ -3,8 +3,32 @@ import path from "path";
 
 const sampleInput = parseFile(path.join(__dirname, "sample.txt"));
 
-console.log(sampleInput);
-
 it("tests something", () => {
-	expect(1).toBe(1);
+	expect(getHeadPositions(sampleInput)).toBe([
+		[0, 0],
+		[1, 0],
+		[2, 0],
+		[3, 0],
+		[4, 0],
+		[4, 1],
+		[4, 2],
+		[4, 3],
+		[4, 4],
+		[3, 4],
+		[2, 4],
+		[1, 4],
+		[1, 3],
+		[2, 3],
+		[3, 3],
+		[4, 3],
+		[5, 3],
+		[5, 2],
+		[4, 2],
+		[3, 2],
+		[2, 2],
+		[1, 2],
+		[0, 2],
+		[1, 2],
+		[2, 2],
+	]);
 });
--- src/09/solution.ts
+++ src/09/solution.ts
@@ -1,14 +1,51 @@
 //https://adventofcode.com/2022/day/9
 import { readFileSync } from "fs";
 
-const parseFile = (path: string) =>
-	readFileSync(path, "utf-8").trim()
+const parseFile = (path: string): [string, number][] =>
+	readFileSync(path, "utf-8")
+		.trim()
 		.split("\n")
-		.map(line => line.split(" "))
+		.map((line) => line.split(" "))
 		.map(([dir, val]) => [dir, Number(val)]);
 
-const getHeadPositions = () => {
-	return [];
+const handleInstruction = (
+	[dir, amount]: [string, number],
+	[x0, y0]: [number, number]
+) => {
+	const result = [];
+	let [x, y] = [x0, y0];
+	for (let i = 0; i <= amount; i++) {
+		switch (dir) {
+		case "U":
+			result.push([x, y++]);
+			break;
+		case "R":
+			result.push([x++, y]);
+			break;
+		case "D":
+			result.push([x, y--]);
+			break;
+		case "L":
+			result.push([x--, y]);
+			break;
+		default:
+			throw new Error("Invalid direction");
+		}
+	}
+	return result as [number, number][];
+};
+const getHeadPositions = (instructions: [string, number][]) => {
+	let history: [number, number][] = [
+		[0, 0]
+	];
+	for (const [dir, val] of instructions) {
+		const last = history.slice(-1)[0];
+		const travelled = handleInstruction([dir, val], last);
+		console.log(travelled);
+		history = [...history, ...travelled.slice(1)];
+	}
+
+	return history;
 };
 
 export { getHeadPositions, parseFile };
